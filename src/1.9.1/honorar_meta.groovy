901e0bb3866bab02ef86c09cf1607aef27008aa3
j-dimension <info@j-lawyer.org>
2019-04-27 21:10:23 +0200

    Version up for honorar plugin. Relates to #56.

--- src/1.9.1/honorar_meta.groovy
+++ src/1.9.1/honorar_meta.groovy
@@ -664,9 +664,9 @@ For more information on this, and how to apply and follow the GNU AGPL, see
 
 name = "Rechner Honorarvereinbarung (in Entwicklung)"
 description = "j-lawyer.org Rechner Honorarvereinbarungen (in Entwicklung)"
-version = "1.1.0";
-author = "Anatol Anuschewski"
-updated = "14.04.2019"
+version = "1.1.1";
+author = "Anatol Anuschewski, Jens Kutschke"
+updated = "27.04.2019"
 supportedPlaceHolders = "{{RVG_TABELLE}}, {{RVG}}"
 
 
