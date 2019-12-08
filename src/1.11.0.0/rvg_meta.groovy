1fa8e1c3c9a809019337e28e38bc24f61da1d9ad
an-mus <anatol@mailbox.org>
2019-12-08 21:56:07 +0100

    Umbenennung RVG-Rechner

--- src/1.11.0.0/rvg_meta.groovy
+++ src/1.11.0.0/rvg_meta.groovy
@@ -662,8 +662,8 @@ For more information on this, and how to apply and follow the GNU AGPL, see
 <https://www.gnu.org/licenses/>.
 */
 
-name = "RVG-Rechner"
-description = "j-lawyer.org RVG-Rechner"
+name = "Rechner RVG Wertgebühren"
+description = "j-lawyer.org Rechner RVG Wertgebühren"
 version = "2.0.1";
 author = "Anatol Anuschewski, Jens Kutschke"
 updated = "18.11.2019"
